186904e48f8038b4c49d8f121ddde799721099dc
Daniyar <fyd178@gmail.com>
2025-05-02 22:28:47 +0500

    2

--- config/config.js
+++ config/config.js
@@ -1,32 +1,31 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-    production: {
-        username: process.env.PROD_DB_USERNAME,
-        password: process.env.PROD_DB_PASSWORD,
-        database: process.env.PROD_DB_DATABASE,
-        host: process.env.PROD_DB_HOST,
-        port: process.env.PROD_DB_PORT,
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-                ca: fs.readFileSync(path.resolve("config", "ca-certificate.crt")).toString()
-            }
-        }
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_DATABASE,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+        ca: fs
+          .readFileSync(path.resolve(__dirname, "ca-certificate.crt"))
+          .toString(),
+      },
     },
-    development: {
-        username: process.env.DEV_DB_USERNAME,
-        password: process.env.DEV_DB_PASSWORD,
-        database: process.env.DEV_DB_DATABASE,
-        host: process.env.DEV_DB_HOST,
-        port: process.env.DEV_DB_PORT,
-        dialect: 'postgres'
-    }
+  },
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_DATABASE,
+    host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
+    dialect: "postgres",
+  },
 };
-
-
-
